5014327198e94be9071eac3437ad623ba5da1053
HubertRonald <hubert.ronald@gmail.com>
2023-08-03 20:26:42 -0500

    fix deleted .gitkeep files

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@
 # file .env has default environment variables
 source $(pwd)/.env
 
+rm -rf ./data/pgdata/.gitkeep;
+rm -rf ./data/pgadmin_data/.gitkeep;
 
 # Create and up detach Service Container
 docker-compose up -d
